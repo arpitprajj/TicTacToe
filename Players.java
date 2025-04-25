9f9d6a517c243f184d19552ac4cd95937a5f80f2
arpitprajj <arpitprajjwal17361@gmail.com>
2025-04-25 23:58:35 +0530

    setting methods in players

---
+++ Players.java
@@ -0,0 +1,24 @@
+public class Players {
+
+    public String name;
+    public PlayingPiece playingPiece;
+
+    public Players(String name,PlayingPiece playingPiece){
+        this.name=name;
+        this.playingPiece=playingPiece;
+    }
+   public String getName(){
+    return name;
+   } 
+   public void setName(String name)
+    {
+        this.name=name;
+    }
+    public PlayingPiece getPlayingPiece(){
+        return playingPiece;
+
+    }
+    public void setPlayingPiece(PlayingPiece playingPiece){
+        this.playingPiece=playingPiece;
+    }
+}
